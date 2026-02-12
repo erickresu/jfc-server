20b9055a7a70a4f8462c790b94b1458f173b14fd
ErickResu <bosserickr@gmail.com>
2026-02-12 16:17:30 +0800

    Implement authentication #2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// GAGAWA KA MUNA NG TABLE …..
+// MIGRATION - IUP YUNG MGA TABLE NA GINAWA
+// .
+// .
+// GAGAWA KA NA NG MODEL
+// .
+// .
+
+// php artisan optimize:clear
+// php artisan config:clear
+// php artisan route:clear
+// php artisan route:list
+// php artisan serve
